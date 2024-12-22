66b5ae14e9560089af1261408af6d240ca4b9ca8
Ank-pro <ank.pro0115@gmail.com>
2024-12-22 18:23:35 +0530

    link tg

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import "./home.css";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
